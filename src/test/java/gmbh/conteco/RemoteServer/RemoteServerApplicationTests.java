bd6e2e38213fa3539133f1de0f38fa8928555231
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-05-03 14:05:32 +0200

    Initial Commit

---
+++ src/test/java/gmbh/conteco/RemoteServer/RemoteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package gmbh.conteco.RemoteServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
